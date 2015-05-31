ada5cb18953fe84e9e5b0ce6b96a76132d1d1479
Nicolas Rolland <nicolas.rolland@gmail.com>
2015-05-31 22:15:46 +0200

    update

--- config.nix
+++ config.nix
@@ -34,30 +34,54 @@ rec {
     haskell784Packages = myHaskellPackages784(myHaskellPackages super.haskell-ng.packages.ghc784);
     haskell763Packages = myHaskellPackages super.haskell-ng.packages.ghc763;
 
-    #defines a new attribute called hsEnv that you can subsequently
-    #install into an environment by saying
-    # $ nix-env -iA nixpkgs.hsEnv  #nixpkgs se refere a une expression ou un repertoire
-    #hsEnv = pkgs.haskellPackages_ghc784.ghcWithPackages (p: with p;
     hs784  = haskell784Packages.ghcWithPackages (p: with p;
-            (haskellDev p) 
-            #++(myPackages p)
-            #[]
-             );
+             [
+                  ghc-mod 
+                  hdevtools
+                  hlint
+                  cabal2nix
+                  aeson base bytestring heist lens MonadCatchIO-transformers mtl
+                  postgresql-simple snap snap-core snap-loader-static snap-server
+                  snaplet-postgresql-simple text time xmlhtml
+                  codex
+                  hobbes
+                  hasktags
+                  djinn #mueval
+                  #idris
+                  stylish-haskell
+                  threadscope
+                  ghc-events
+                  timeplot splot
+                  hakyll
+            ]
+            ++(myPackages p)
+         );
     hs7101 = haskellngPackages.ghcWithPackages (p: with p;
-            (haskellDev p) 
-            ++(myPackages p) 
+              [
+                  #ghc-mod
+                  hdevtools
+                  hlint
+                  cabal2nix
+                  aeson base bytestring heist lens MonadCatchIO-transformers mtl
+                  postgresql-simple snap snap-core snap-loader-static snap-server
+                  snaplet-postgresql-simple text time xmlhtml
+                  codex
+                  hobbes
+                  hasktags
+                  #djinn mueval
+                  #idris
+                  #stylish-haskell
+                  #threadscope
+                  #ghc-events
+                  #timeplot #splot
+                  hakyll
+            ] 
+            ++( myPackages7101  p) 
          );
 
     hsEnvHoogle = withHoogle hs784;
     hsEmpty = pkgs.haskell-ng.packages.ghc784.ghcWithPackages (p: with p; []);
 
-    #haskellngPackages = pkgs.haskellngPackages.override {
-    #    overrides = self: super: {
-    #    cmdtheline = self.callPackage  ../hask/purescript-nix/cmdtheline.nix {};
-    #    purescript = self.callPackage  ../hask/purescript-nix/purescript.nix {};
-    #    #reflex-dom = self.callPackage  ../hask/try-reflex/reflex-dom/default.nix {};
-    #   };
-    #  };
     devWeb = let haskellngPackages = pkgs.haskellngPackages.override {
               extension = self: super: {
                cmdtheline = self.callPackage ../hask/purescript-nix/cmdtheline.nix {};
@@ -78,6 +102,7 @@ rec {
                               git
                             ];
        };
+       
     agdaEnv = pkgs.myEnvFun {
             name =  "agda";
             buildInputs = [
@@ -104,36 +129,9 @@ rec {
       };
   };#end packageOverrides
 
-  allowBroken = true;
-  allowUnfree = true;
-  
-  haskellDev = p: with p; [
-     #ghc
-     ghc-mod
-     hdevtools
-     hlint
-     cabal2nix
-     aeson base bytestring heist lens MonadCatchIO-transformers mtl
-     #postgresql-simple snap snap-core snap-loader-static snap-server
-     #snaplet-postgresql-simple text time xmlhtml
-     #codex
-     #hobbes
-     #hasktags
-     #djinn mueval
-     #idris
-     #stylish-haskell
-     #threadscope
-     ghc-events
-     timeplot splot
-     hakyll
-      ];
 
-  extra = p : with p; [
-     ihaskell
-  ];
-  
-  myPackages = p: with p; [
-    #classy-prelude-yesod
+  myPackages7101 = p: with p; [
+     #classy-prelude-yesod
   #stm
   #wai-loggerm
   #yesod
@@ -142,6 +140,129 @@ rec {
   #yesod-core
   #yesod-for
   #yesod-static
+  HUnit
+  IfElse
+  MemoTrie
+  MissingH
+  QuickCheck 
+  accelerate
+  adjunctions
+  aeson
+  async
+  attempt
+  attoparsec
+  bifunctors
+  bytestring
+  cassava
+  classy-prelude
+  classy-prelude-conduit
+  clay 
+  clay 
+  conduit
+  containers
+  data-default
+  derive
+  directory
+  distributive
+  dlist
+  dns
+  doctest
+  either
+  errors
+  exceptions
+  failure
+  fast-logger
+  feed
+  file-embed
+  #ghcjs-dom
+  hamlet
+  hashable
+  hashtables
+  haskeline
+  hfsevents
+  hjsmin
+  hoopl
+  hslogger
+  hspec
+  html
+  http-conduit
+  iso8601-time
+  lens
+  lens
+  list-tries
+  lucid
+  mmorph
+  monad-control
+  monad-logger
+  mtl
+  mvc
+  pandoc
+  parallel
+  parsec
+  persistent
+  persistent-postgresql
+  persistent-template
+  pipes
+  pipes-concurrency
+  pointed
+  profunctors
+  random
+  reducers
+  reflection
+  #reflex
+  #reflex-dom
+  #reflex-todomvc
+  resourcet
+  retry
+  rex
+  safe
+  sbv
+  scotty
+  semigroupoids
+  semigroups
+  shake
+  shakespeare
+  shelly
+  speculation
+  split
+  spoon
+  strict
+  strptime
+  syb
+  system-fileio
+  tagged
+  tar
+  tasty
+  template-haskell
+  text
+  text         
+  text           
+  time
+  #timeparsers
+  transformers
+  unordered-containers
+  vector
+  void
+  wai
+  wai-extra
+  warp
+  xhtml
+  yaml
+  zippers
+  zlib
+
+  ];
+  
+  myPackages = p: with p; [
+    #classy-prelude-yesod
+  stm
+  #wai-loggerm
+  yesod
+  yesod-auth
+  yesod-bin
+  yesod-core
+  #yesod-for
+  yesod-static
   HUnit
   IfElse
   MemoTrie
@@ -255,5 +376,8 @@ rec {
 # z3
 #cabal-install
   ];
+
+  allowBroken = true;
+  allowUnfree = true;
 
 }
